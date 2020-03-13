a6d08e66c8cf4c1534836c94d918ca7bb3db9c36
harrel56 <marcin.slowik95@gmail.com>
2020-03-13 18:56:06 +0000

    webpack scss

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>HELO</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>elosXsssssssXXXXXss</h1>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,48 @@
 var path = require('path');
 
 module.exports = {
-    entry: {
-        index: './src/main/js/index.js'
-    },
-    devtool: 'sourcemaps',
-    cache: true,
-    mode: 'development',
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/build/bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
-                }]
-            }
-        ]
-    }
+	entry: ['./src/main/js/index.js', './src/main/resources/scss/main.scss'],
+	devtool: 'sourcemaps',
+	cache: true,
+	mode: 'development',
+	output: {
+		path: __dirname,
+		filename: './src/main/resources/static/build/bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react']
+						}
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}
+		]
+	}
 };
